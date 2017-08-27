e3d36c3350d8d4bafcda3a263f3786c547a0c0ce
JohnMcWilliams <johnmcwilliams8@hotmail.com>
2017-08-27 12:56:48 +0100

    add worksheet to excel class

--- excel.rb
+++ excel.rb
@@ -1,7 +1,8 @@
 class Excel
-  attr_accessor :workbook, :filepath
+  attr_accessor :workbook, :filepath, :worksheet
   @workbook = nil
   @filepath = nil
+  @worksheet = nil
   def save_file
     @workbook.write(@filepath)
   end
