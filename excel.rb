076a6d72b7195bd3285ba68f44874f38a30591fe
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2017-09-02 17:17:17 +0100

    fix conversion of column string to index

--- excel.rb
+++ excel.rb
@@ -32,6 +32,7 @@ class Excel
   def rubyxl_cells(rubyxl_worksheet, worksheet)
     worksheet.each do |cell_key, attributes|
       row_index, column_index = cell_key_to_coordinates(cell_key)
+      puts column_index
       rubyxl_worksheet.add_cell(row_index, column_index, attributes[:value])
       rubyxl_worksheet[row_index][column_index].change_fill(attributes[:fill])
     end
@@ -41,7 +42,7 @@ class Excel
     row_start_index = cell_key =~ /\d+/
     column_string = cell_key[0..row_start_index - 1]
     value = ('A'..'Z').map.with_index.to_h
-    column_index = column_string.chars.inject(0) { |sum, current| sum * 26 + value[current] }
+    column_index = column_string.chars.inject(0) { |sum, current| sum * 26 + value[current] + 1 } - 1
     row_index = cell_key[row_start_index..-1].to_i - 1
     [row_index, column_index]
   end
