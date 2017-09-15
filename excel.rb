fffbe62a41d21c3ebdf44599ea370de9b6f37ce0
JohnMcWilliams <johnmcwilliams8@hotmail.com>
2017-09-15 17:15:56 +0100

    complete basic layout and formatting of worksheets

--- excel.rb
+++ excel.rb
@@ -33,29 +33,31 @@ class Excel
 
   def rubyxl_cells(rubyxl_worksheet, worksheet)
     worksheet.each do |cell_key, attributes|
-      (0..253).to_a.each do |item|
-        rubyxl_worksheet.change_column_font_name(item, 'Consolas')
-        rubyxl_worksheet.change_column_font_size(item, 11)
-      end
-      rubyxl_worksheet.change_column_width(1, 40)
-      rubyxl_worksheet.change_column_width(2, 20)
-      rubyxl_worksheet.change_column_width(3, 20)
-      rubyxl_worksheet.change_row_font_size(0, 13)
-      rubyxl_worksheet.change_row_font_size(1, 12)
-
       row_index, column_index = RubyXL::Reference.ref2ind(cell_key)
-      rubyxl_worksheet.add_cell(row_index, column_index, attributes[:value])
-      rubyxl_worksheet.merge_cells(0, 0, 0, 4) if attributes[:merge]
-      rubyxl_worksheet.merge_cells(4, 0, 4, 4) if attributes[:merge]
-      rubyxl_worksheet.merge_cells(7, 0, 7, 4) if attributes[:merge]
-      rubyxl_worksheet.add_cell(row_index, column_index, attributes[:sum]) if attributes[:sum]
+
+      index_b, index_a = RubyXL::Reference.ref2ind(attributes[:merge]) if attributes[:merge]
+      rubyxl_worksheet.merge_cells(row_index, column_index, index_a, index_b) if attributes[:merge]
+
+      if attributes[:formula]
+        rubyxl_worksheet.add_cell(row_index, column_index, '', attributes[:formula]).set_number_format '0.00'
+      else
+        rubyxl_worksheet.add_cell(row_index, column_index, attributes[:value])
+      end
+
+      rubyxl_worksheet[row_index][column_index].change_contents(attributes[:sum], rubyxl_worksheet[row_index][column_index].formula) if attributes[:sum]
+      rubyxl_worksheet.change_column_width(column_index, attributes[:width]) if attributes[:width]
+
+      rubyxl_worksheet.change_row_font_name(row_index, attributes[:name]) if attributes[:name]
+      rubyxl_worksheet.change_row_font_size(row_index, attributes[:size])  if attributes[:size]
+
+
       rubyxl_worksheet[row_index][column_index].set_number_format(attributes[:format]) if attributes[:format]
       rubyxl_worksheet[row_index][column_index].change_fill(attributes[:fill]) if attributes[:fill]
       rubyxl_worksheet[row_index][column_index].change_horizontal_alignment(attributes[:align]) if attributes[:align]
       rubyxl_worksheet[row_index][column_index].set_number_format(attributes[:format]) if attributes[:format]
       rubyxl_worksheet[row_index][column_index].change_font_bold(attributes[:bold]) if attributes[:bold]
 
-      # change_border_all???
+      # needs a: change_border_all
       rubyxl_worksheet[row_index][column_index].change_border('top' , attributes[:border_top]) if attributes[:border_top]
       rubyxl_worksheet[row_index][column_index].change_border('bottom' , attributes[:border_bottom]) if attributes[:border_bottom]
       rubyxl_worksheet[row_index][column_index].change_border('left' , attributes[:border_left]) if attributes[:border_left]
--- excel_hash.rb
+++ excel_hash.rb
@@ -6,24 +6,53 @@ filepath = "tempxlsx/file_#{timestamp}.xlsx"
 
 workbook = {
     filepath: filepath,
-    # Sheet1
+    # Sheet1: Master Sheet
     'A1.Cash' => {
         'A1' => {
             value: 'Cash',
-            font: 'consolas',
+            name: 'consolas',
             fill: 'c0c0c0',
             align: 'center',
             bold: true,
-            merge: ''
+            merge: 'A5',
+            size: 13
         },
         'A2' => {
             value: 'Date',
+            name: 'consolas',
             align: 'center',
             fill: 'c0c0c0',
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            size: 12
+        },
+        'A3' => {
+            name: 'consolas',
+            size: 11
+        },
+        'A4' => {
+            name: 'consolas',
+            size: 11
+        },
+        'A5' => {
+            fill: '808080',
+            merge: 'E5'
+        },
+        'A6' => {
+            name: 'consolas',
+            size: 11,
+            fill: 'c0c0c0'
+        },
+        'A7' => {
+            name: 'consolas',
+            size: 11,
+            fill: 'c0c0c0'
+        },
+        'A8' => {
+            fill: '808080',
+            merge: 'H5'
         },
         'B2' => {
             value: 'Description',
@@ -32,7 +61,21 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 40
+        },
+        'B5' => {
+            fill: '808080'
+        },
+        'B6' => {
+            value: 'Totals',
+            align: 'right',
+            fill: 'c0c0c0'
+        },
+        'B7' => {
+            value: 'Balance',
+            align: 'right',
+            fill: 'c0c0c0'
         },
         'C2' => {
             value: 'Dr',
@@ -41,7 +84,21 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
+        },
+        'C5' => {
+            fill: '808080'
+        },
+        'C6' => {
+            formula: '=sum(C2:C5)',
+            align: 'right',
+            fill: 'c0c0c0'
+        },
+        'C7' => {
+            formula: '=IF(C6-D6>0, C6-D6, 0)',
+            align: 'right',
+            fill: 'c0c0c0'
         },
         'D2' => {
             value: 'Cr',
@@ -50,7 +107,21 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
+        },
+        'D5' => {
+            fill: '808080'
+        },
+        'D6' => {
+            formula: '=SUM(D2:D5)',
+            align: 'right',
+            fill: 'c0c0c0'
+        },
+        'D7' => {
+            formula: '=IF(D6-C6>0, D6-C6, 0)',
+            align: 'right',
+            fill: 'c0c0c0'
         },
         'E2' => {
             value: 'Balance',
@@ -62,93 +133,44 @@ workbook = {
             border_right: 'thin'
         },
         'E3' => {
-            sum: '=C3-D3',
+            formula: '=(C3-D3)',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
         },
         'E4' => {
-            sum: '=E3+C4-D4',
+            formula: '=E3+C4-D4',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
         },
-        'A5' => {
-            fill: '808080'
-        },
-        'B5' => {
-            fill: '808080'
-        },
-        'C5' => {
-            fill: '808080'
-        },
-        'D5' => {
-            fill: '808080'
-        },
         'E5' => {
             fill: '808080'
-        },
-        'A6' => {
-            fill: 'c0c0c0'
-        },
-        'B6' => {
-            value: 'Totals',
-            align: 'right',
-            fill: 'c0c0c0'
-        },
-        'C6' => {
-            value: '=SUM(C2:C5)',
-            align: 'right',
-            fill: 'c0c0c0'
-        },
-        'D6' => {
-            value: '=SUM(D2:D5)',
-            align: 'right',
-            fill: 'c0c0c0'
         },
         'E6' => {
             fill: 'c0c0c0'
         },
-        'A7' => {
-            fill: 'c0c0c0'
-        },
-        'B7' => {
-            value: 'Balance',
-            align: 'right',
-            fill: 'c0c0c0'
-        },
-        'C7' => {
-            value: '=IF(C6-D6>0, C6-D6, 0)',
-            align: 'right',
-            fill: 'c0c0c0'
-        },
-        'D7' => {
-            value: '=IF(D6-C6>0, D6-C6, 0)',
-            align: 'right',
-            fill: 'c0c0c0'
-        },
         'E7' => {
-            value: '=C7-D7',
+            formula: '=C7-D7',
             align: 'right',
             fill: 'c0c0c0'
-        },
-        'A8' => {
-            fill: '808080'
         }
     },
     # Sheet2
     'A2.AR' => {
         'A1' => {
-            value: 'Acccounts Receivable',
-            font: 'consolas',
+            name: 'consolas',
+            value: 'Accounts Receivable',
             fill: 'c0c0c0',
             align: 'center',
             bold: true,
-            merge: ''
+            merge: 'A5',
+            resize: 13
         },
         'A2' => {
+            name: 'consolas',
             value: 'Date',
             align: 'center',
             fill: 'c0c0c0',
@@ -157,6 +179,14 @@ workbook = {
             border_left: 'thin',
             border_right: 'thin'
         },
+        'A5' => {
+            fill: '808080',
+            merge: 'E5'
+        },
+        'A8' => {
+            fill: '808080',
+            merge: 'H5'
+        },
         'B2' => {
             value: 'Description',
             align: 'center',
@@ -164,7 +194,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 40
         },
         'C2' => {
             value: 'Dr',
@@ -173,7 +204,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
         },
         'D2' => {
             value: 'Cr',
@@ -182,7 +214,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
         },
         'E2' => {
             value: 'Balance',
@@ -194,21 +227,18 @@ workbook = {
             border_right: 'thin'
         },
         'E3' => {
-            sum: '=C3-D3',
+            formula: '=C3-D3',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
         },
         'E4' => {
-            sum: '=E3+C4-D4',
+            formula: '=E3+C4-D4',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
-        },
-        'A5' => {
-            fill: '808080'
         },
         'B5' => {
             fill: '808080'
@@ -265,20 +295,21 @@ workbook = {
             value: '=C7-D7',
             align: 'right',
             fill: 'c0c0c0'
-        },
-        'A8' => {
-            fill: '808080'
-        }    },    # Sheet3
+        }
+    },
+    # Sheet3
     'L1.CT' => {
         'A1' => {
+            name: 'consolas',
             value: 'Corporation Tax',
-            font: 'consolas',
             fill: 'c0c0c0',
             align: 'center',
             bold: true,
-            merge: ''
+            merge: 'A5',
+            resize: 13
         },
         'A2' => {
+            name: 'consolas',
             value: 'Date',
             align: 'center',
             fill: 'c0c0c0',
@@ -287,6 +318,14 @@ workbook = {
             border_left: 'thin',
             border_right: 'thin'
         },
+        'A5' => {
+            fill: '808080',
+            merge: 'E5'
+        },
+        'A8' => {
+            fill: '808080',
+            merge: 'H5'
+        },
         'B2' => {
             value: 'Description',
             align: 'center',
@@ -294,7 +333,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 40
         },
         'C2' => {
             value: 'Dr',
@@ -303,7 +343,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
         },
         'D2' => {
             value: 'Cr',
@@ -312,7 +353,8 @@ workbook = {
             border_top: 'thin',
             border_bottom: 'thin',
             border_left: 'thin',
-            border_right: 'thin'
+            border_right: 'thin',
+            width: 20
         },
         'E2' => {
             value: 'Balance',
@@ -324,21 +366,18 @@ workbook = {
             border_right: 'thin'
         },
         'E3' => {
-            sum: '=C3-D3',
+            formula: '=C3-D3',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
         },
         'E4' => {
-            sum: '=E3+C4-D4',
+            formula: '=E3+C4-D4',
             format: '0.00',
             align: 'right',
             font: 'consolas',
             fill: 'FFFFFF'
-        },
-        'A5' => {
-            fill: '808080'
         },
         'B5' => {
             fill: '808080'
@@ -395,9 +434,6 @@ workbook = {
             value: '=C7-D7',
             align: 'right',
             fill: 'c0c0c0'
-        },
-        'A8' => {
-            fill: '808080'
         }
     }
 }
