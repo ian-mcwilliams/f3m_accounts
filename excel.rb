09a5dcc406ba17ee38155009ebf165fcb200eb05
JohnMcWilliams <johnmcwilliams8@hotmail.com>
2017-11-11 17:36:37 +0000

    set the extents and format of the worksheet and refactor excel.rb, add initial validation methods

--- excel.rb
+++ excel.rb
@@ -4,16 +4,27 @@ class Excel
   attr_accessor :hash_workbook
 
   def initialize(source: nil)
-    if source.class == Hash
+    if source.is_a?(Hash)
       @hash_workbook = source
-    elsif source.class == String
+    elsif source.ia_a?(String)
       read_file(source)
     end
   end
 
+  def validate_hash_workbook
+    raise("@hash_workbook is class '#{@hash_workbook.class}', should be a Hash") unless @hash_workbook.is_a?(Hash)
+    @validation = []
+    @hash_workbook.each { |hash_worksheet_name, hash_worksheet| validate_hash_worksheet(hash_worksheet_name, hash_worksheet) }
+    raise(@validation.join("\n")) unless @validation.empty?
+  end
+
+  def validate_hash_worksheet(hash_worksheet_name, hash_worksheet)
+    @validation << "hash_worksheet_name is class '#{hash_worksheet_name.class}', should be a String" unless hash_worksheet_name.is_a?(String)
+    # other validation...
+  end
+
   def save_file(filepath)
-    # rubyxl_workbook = hash_workbook_to_rubyxl_workbook
-    # rubyxl_workbook.write(@hash_workbook[:filepath])
+    validate_hash_workbook
     hash_workbook_to_rubyxl_workbook.write(filepath)
   end
 
@@ -32,41 +43,54 @@ class Excel
     rubyxl_workbook
   end
 
-  def hash_worksheet_to_rubyxl_worksheet(hash_worksheet, rubyxl_worksheet)
-
+  def set_hash_worksheet_extents(hash_worksheet)
+    row_keys = hash_worksheet[:rows].keys.map { |item| "A#{item}" }
+    column_keys = hash_worksheet[:columns].keys.map { |item| "#{item}1" }
     hash_worksheet[:row_count] = 0
     hash_worksheet[:column_count] = 0
-    hash_worksheet[:cells].keys.each do |hash_cell_key|
+    (hash_worksheet[:cells].keys + row_keys + column_keys).each do |hash_cell_key|
       row_index, column_index = RubyXL::Reference.ref2ind(hash_cell_key)
-      hash_worksheet[:row_count] = row_index + 1 if row_index + 1 > hash_worksheet[:row_count]
-      hash_worksheet[:column_count] = column_index + 1 if column_index + 1 > hash_worksheet[:column_count]
-    end
-
-    process_sheet_to_populated_block(hash_worksheet)
-    hash_row_column(hash_worksheet)
-
-    hash_worksheet[:cells].each do |hash_cell_key, combined_hash_cell|
-      combined_hash_cell = hash_worksheet[:worksheet].merge(combined_hash_cell)
-      hash_cell_to_rubyxl_cell(hash_cell_key, combined_hash_cell, rubyxl_worksheet)
+      hash_worksheet[:row_count] = row_index + 1 if row_index >= hash_worksheet[:row_count]
+      hash_worksheet[:column_count] = column_index + 1 if column_index >= hash_worksheet[:column_count]
     end
   end
 
-  def hash_cell_to_rubyxl_cell(hash_cell_key, combined_hash_cell, rubyxl_worksheet)
-    row_index, column_index = RubyXL::Reference.ref2ind(hash_cell_key)
+  def hash_worksheet_to_rubyxl_worksheet(hash_worksheet, rubyxl_worksheet)
+    process_sheet_to_populated_block(hash_worksheet)
+    hash_worksheet[:cells].sort.each do |hash_cell_key, hash_cell|
+      combined_hash_cell = get_combined_hash_cell(hash_worksheet, hash_cell_key, hash_cell)
+      row_index, column_index = RubyXL::Reference.ref2ind(hash_cell_key)
+      add_rubyxl_cells(combined_hash_cell, rubyxl_worksheet, row_index, column_index)
+      hash_cell_to_rubyxl_cell(combined_hash_cell, rubyxl_worksheet, row_index, column_index)
+    end
+  end
 
-    index_b, index_a = RubyXL::Reference.ref2ind(combined_hash_cell[:merge])
-    rubyxl_worksheet.merge_cells(row_index, column_index, index_a, index_b) if combined_hash_cell[:merge]
+  def get_combined_hash_cell(hash_worksheet, hash_cell_key, hash_cell)
+    # first get data from the matching column if it's specified
+    column_keys = hash_worksheet[:columns].keys.select { |key| hash_cell_key =~ /^#{key}\d+$/ }
+    column_keys.empty? ? hash_column = {} : hash_column = hash_worksheet[:columns][column_keys[0]]
+    combined_hash_cell = hash_column.merge(hash_cell)
+    # then get data from the matching row if it's specified
+    row_keys = hash_worksheet[:rows].keys.select { |key| hash_cell_key =~ /^\D+#{key}$/ }
+    row_keys.empty? ? hash_row = {} : hash_row = hash_worksheet[:rows][row_keys[0]]
+    combined_hash_cell = hash_row.merge(combined_hash_cell)
+    hash_worksheet[:worksheet].merge(combined_hash_cell)
+  end
+
+  def add_rubyxl_cells(combined_hash_cell, rubyxl_worksheet, row_index, column_index)
     if combined_hash_cell[:formula]
       rubyxl_worksheet.add_cell(row_index, column_index, '', combined_hash_cell[:formula]).set_number_format combined_hash_cell[:dp_2]
     else
       rubyxl_worksheet.add_cell(row_index, column_index, combined_hash_cell[:value])
-      rubyxl_worksheet.change_row_fill(row_index, combined_hash_cell[:fill])  if combined_hash_cell[:fill]
     end
+  end
 
+  def hash_cell_to_rubyxl_cell(combined_hash_cell, rubyxl_worksheet, row_index, column_index)
+    merge_row_index, merge_column_index = RubyXL::Reference.ref2ind(combined_hash_cell[:merge])
+
+    rubyxl_worksheet.merge_cells(row_index, column_index, merge_column_index, merge_row_index) if combined_hash_cell[:merge]
     rubyxl_worksheet.change_column_width(column_index, combined_hash_cell[:width])  if combined_hash_cell[:width]
 
-    # rubyxl_worksheet[row_index][column_index].change_contents(hash_cell[:sum], rubyxl_worksheet[row_index][column_index].formula) if hash_cell[:sum]
-    # rubyxl_worksheet[row_index][column_index].set_number_format(hash_cell[:format]) if hash_cell[:format]
     rubyxl_worksheet[row_index][column_index].change_font_name(combined_hash_cell[:font_style]) if combined_hash_cell[:font_style]
     rubyxl_worksheet[row_index][column_index].change_font_size(combined_hash_cell[:font_size]) if combined_hash_cell[:font_size]
     rubyxl_worksheet[row_index][column_index].change_fill(combined_hash_cell[:fill]) if combined_hash_cell[:fill]
@@ -80,6 +104,7 @@ class Excel
       rubyxl_worksheet[row_index][column_index].change_border('right' , combined_hash_cell[:border_all])
     end
   end
+
   def read_file(path)
     rubyxl_workbook = RubyXL::Parser.parse(path)
     @hash_workbook = rubyxl_to_hash(rubyxl_workbook)
@@ -115,6 +140,7 @@ class Excel
   end
 
   def process_sheet_to_populated_block(hash_worksheet)
+    set_hash_worksheet_extents(hash_worksheet)
     hash_worksheet[:row_count].times do |row_index|
       hash_worksheet[:column_count].times do |column_index|
         cell_key = RubyXL::Reference.ind2ref(row_index, column_index)
@@ -123,22 +149,9 @@ class Excel
     end
   end
 
-  def hash_row_column(hash_worksheet)
-    hash_row = Hash.new{|hsh,row| hsh[row] = [] }
-    hash_column = Hash.new{|hsh,column| hsh[column] = [] }
-    hash_worksheet[:row_count].times do |row_index|
-      row_key = RubyXL::Reference.ind2ref(row_index)
-      row_key = /\d+/.match(row_key)
-      hash_row['rows'].push row_index
-      hash_row['rows'].push row_key
-    end
-    hash_worksheet[:column_count].times do |column_index|
-      column_key = RubyXL::Reference.ind2ref(0, column_index)
-      column_key = /\D+/.match(column_key)
-      hash_column['columns'].push column_index
-      hash_column['columns'].push column_key
-    end
-    puts [hash_row]
-    puts [hash_column]
+  def excel_col_index(hash_worksheet)
+    column_no = ()
+    value = Hash[ ('A'..'Z').map.with_index.to_a ]
+    hash_worksheet[column_no] = hash_worksheet[:columns][:column_ref].chars.inject(0){ |x,c| x*26 + value[c] + 1}
   end
 end
