8236990cecadd8d686cd66ee76b21d0a638a0d38
JohnMcWilliams <johnmcwilliams8@hotmail.com>
2017-11-12 14:16:17 +0000

    create sample_worksheet file and add nil and array inputs for excel class

--- excel.rb
+++ excel.rb
@@ -3,11 +3,23 @@ require 'rubyXL'
 class Excel
   attr_accessor :hash_workbook
 
+  # class generates based on input type:
+  # pass in a string to read from that file location
+  # pass in a hash to set @hash_workbook to that hash
+  # pass in nil for @hash_workbook to be an empty hash
+  # pass in an array of strings to generate empty worksheets at initialisation where the strings are tab names
   def initialize(source: nil)
     if source.is_a?(Hash)
       @hash_workbook = source
-    elsif source.ia_a?(String)
+    elsif source.is_a?(String)
       read_file(source)
+    elsif source.nil?
+      @hash_workbook = {}
+    elsif source.is_a?(Array)
+      @hash_workbook = {}
+      source.each { |sheet_name| @hash_workbook.update({sheet_name => {}}) }
+    else
+      raise("source argument of class '#{source.class}' not handled by the Excel class")
     end
   end
 
--- excel_hash.rb
+++ excel_hash.rb
@@ -1,122 +1,27 @@
 require 'date'
 require_relative 'excel'
 require 'awesome_print'
+require_relative 'sample_worksheet'
+
+
+sheet_names = %w[A1.Cash A2.AR L1.CT]
+
+excel_object = Excel.new(source: sheet_names)
 
 timestamp = DateTime.now.strftime('%y%m%d_%H%M%S')
 filepath = "tempxlsx/file_#{timestamp}.xlsx"
 
-sheet_names = %w[A1.Cash] # A2.AR L1.CT]
-
-hash_workbook = {}
 sheet_names.each do |sheet_name|
-  current_worksheet = {
-      sheet_name => {
-          worksheet: {
-              font_style: 'Consolas',
-              font_size: 11,
-              dp_2: '0.00',
-              border_all: 'thin'
-          },
-          rows: {
-              '1' => {
-                  font_style: 'Arial',
-                  fill: 'c0c0c0',
-                  align: 'center',
-                  bold: true,
-                  border_all: 'thin'
-              },
-              '2' => {
-                  font_style: 'Arial',
-                  fill: 'c0c0c0',
-                  align: 'center',
-                  bold: true,
-                  border_all: 'thin'
-              },
-              '6' => {
-                  fill: 'c0c0c0',
-                  align: 'right',
-                  border_all: 'none'
-              },
-              '7' => {
-                  fill: 'c0c0c0',
-                  align: 'right',
-                  border_all: 'none'
-              },
-              '5' => {
-                  fill: '808080',
-                  border_all: 'none'
-              },
-              '8' => {
-                  fill: '808080',
-                  border_all: 'none'
-              }
-          },
-          columns: {
-
-          },
-          cells: {
-              'A1' => {
-                  value: sheet_name,
-                  font_size: 13,
-                  merge: 'A5'
-              },
-              'A2' => {
-                  value: 'Date',
-                  font_size: 12
-              },
-              'B2' => {
-                  value: 'Description',
-                  width: 40
-              },
-              'B6' => {
-                  value: 'Totals'
-              },
-              'B7' => {
-                  value: 'Balance'
-              },
-              'C2' => {
-                  value: 'Dr'
-              },
-              'C6' => {
-                  formula: '=sum(C2:C5)',
-                  border_all: 'thin'
-              },
-              'C7' => {
-                  formula: '=IF(C6-D6>0, C6-D6, 0)',
-                  border_all: 'thin'
-              },
-              'D2' => {
-                  value: 'Cr'
-              },
-              'D6' => {
-                  formula: '=SUM(D2:D5)',
-                  border_all: 'thin'
-              },
-              'D7' => {
-                  formula: '=IF(D6-C6>0, D6-C6, 0)',
-                  border_all: 'thin'
-              },
-              'E2' => {
-                  value: 'Balance',
-              },
-              'E3' => {
-                  formula: '=IF(sum(C3:D3)=0,sum(C3:D3), C3-D3)'
-              },
-              'E6' => {
-                  border_all: 'thin'
-              },
-              'E7' => {
-                  formula: '=C7-D7',
-                  border_all: 'thin'
-              },
-          }
-      }
-  }
-  hash_workbook.update(current_worksheet)
+  excel_object.hash_workbook[sheet_name].update(sample_worksheet(sheet_name))
 end
+
 # ap hash_workbook
-excel_object = Excel.new(source: hash_workbook)
 excel_object.save_file(filepath)
+
+
+
+
+
 
 
 # if combined_hash_cell[:balance] || combined_hash_cell[:cr_balance]
