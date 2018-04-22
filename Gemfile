06b1039754f36711b31d6534590723f91f0192c6
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-04-22 13:04:01 +0100

    reset project state after moving excel out to rxl gem

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 ruby '2.3.0'
 
-gem 'rubyXL'
 gem 'awesome_print'
--- spec/support/env.rb
+++ spec/support/env.rb
@@ -1,7 +1,1 @@
 require 'rspec'
-require 'fileutils'
-require 'pathname'
-require_relative 'excel_spec_helpers'
-require_relative '../../excel'
-
-ENV['TEMP_XLSX_PATH'] = 'spec/temp_xlsx'
